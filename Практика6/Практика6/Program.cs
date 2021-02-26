c343e5c85d710c5a30edc4c2449476901375076c
AlexAredov <aaredov@bk.ru>
2021-02-26 22:47:01 +0300

    практикабббб

---
+++ Практика6/Практика6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Практика6
+{
+    
+    class Program
+    {
+        delegate void hello();
+        static void Main(string[] args)
+        {
+            hello hel;
+            hel = Hello.Hello_English;
+            hel();
+            hel = Hello.Hello_French;
+            hel();
+            hel = Hello.Hello_German;
+            hel();
+            hel = Hello.Hello_Italian;
+            hel();
+            hel = Hello.Hello_Russian;
+            hel();
+        }
+    }
+
+    static class Hello
+    {
+        public static void Hello_Russian()
+        {
+            Console.WriteLine("Привет!");
+        }
+        public static void Hello_English()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public static void Hello_German()
+        {
+            Console.WriteLine("Hallo!");
+        }
+        public static void Hello_Italian()
+        {
+            Console.WriteLine("Ciao!");
+        }
+        public static void Hello_French()
+        {
+            Console.WriteLine("Bonjour!");
+        }
+    }
+}
---
+++ Практика6_2/Практика6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Практика6_2
+{
+    class Program
+    {
+        public delegate int Op(int x, int y);
+        static void Main(string[] args)
+        {
+            Op sum = (int x, int y) => x + y;
+            Op mul = (int x, int y) => x * y;
+            Op div = (int x, int y) => x / y;
+            int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine());
+            int s = sum(x, y);
+            int m = mul(x, y);
+            int d = div(x, y);
+            Console.WriteLine($"Сумма: {s}, Произведение: {m}, Частное {d}");
+        }
+    }
+}
